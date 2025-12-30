d4b65994746babbe11b361f32da45b8d8e35630d
Yosuke Sakurai <hawk20605@gmail.com>
2025-12-30 22:17:23 +0900

    Release v1.0.2

--- taskgate-sdk/build.gradle.kts
+++ taskgate-sdk/build.gradle.kts
@@ -43,7 +43,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.taskgate"
             artifactId = "sdk"
-            version = "1.0.1"
+            version = "1.0.2"
 
             afterEvaluate {
                 from(components["release"])
